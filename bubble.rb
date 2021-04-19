0198ffccbb18f32251cfafceaa733cc8030ae7dd
safafa <safaaballagh97@gmail.com>
2021-04-19 14:06:58 +0000

    Fix err

--- bubble.rb
+++ bubble.rb
@@ -5,15 +5,14 @@ def bubble_sort(array)
       unsorted = false
       while i < array.length - 1
         if array[i] > array[i + 1]
-            tmp = array[i]
-            array[i] = array[i + 1]
-            array[i + 1] = tmp
-            unsorted = true
+          tmp = array[i]
+          array[i] = array[i + 1]
+          array[i + 1] = tmp
+          unsorted = true
         end
         i += 1
-      end   
+      end
     end
 
-    return array
-
+    array
 end
