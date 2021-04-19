4e39f6dc61de0f6bac5c0681aa6cb211921fbbf2
Sebastian Rizzuto <srizzuto@gmail.com>
2021-04-19 12:18:03 -0300

    add bubble_sort_by

--- bubble.rb
+++ bubble.rb
@@ -16,3 +16,26 @@ def bubble_sort(array)
 
   array
 end
+
+def bubble_sort_by(array)
+  unsorted = true
+  while unsorted
+    i = 0
+    unsorted = false
+    while i < array.length - 1
+      if (yield array[i], array[i + 1]) >= 0
+        tmp = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = tmp
+        unsorted = true
+      end
+      i += 1
+    end
+  end
+
+  puts array
+end
+
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
+  left.length - right.length
+end
